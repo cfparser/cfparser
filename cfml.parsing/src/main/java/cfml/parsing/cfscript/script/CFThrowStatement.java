51c858c046a8bf00f08cb3731fb519a1a602f434
Ryan <ryaneberly@gmail.com>
2017-02-11 21:25:52 -0500

    CFThrowSTatement is deprecated.  FunctionCall is used instead

--- cfml.parsing/src/main/java/cfml/parsing/cfscript/script/CFThrowStatement.java
+++ cfml.parsing/src/main/java/cfml/parsing/cfscript/script/CFThrowStatement.java
@@ -24,6 +24,7 @@ import java.util.List;
 import cfml.parsing.cfscript.CFExpression;
 import cfml.parsing.util.ArrayBuilder;
 
+@Deprecated
 public class CFThrowStatement extends CFParsedStatement implements java.io.Serializable {
 	
 	private static final long serialVersionUID = 1L;
---
+++ cfml.parsing/src/test/resources/cfml/tests/expressions/throw.cfc
@@ -0,0 +1,5 @@
+throw (message = "message", 
+type = "exception type", detail="xyz", 
+errorCode = "error code", 
+extendedInfo = "additional info", 
+object = "java exception object");
